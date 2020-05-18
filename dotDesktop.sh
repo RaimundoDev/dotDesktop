f50a666a8d7adea5e4c7680e34ed46db5445fa85
Raimundo Nonato <raimundo.nonatotjg18@gmail.com>
2020-05-18 20:24:55 -0300

    Add different files to each function

--- dotDesktop.sh
+++ dotDesktop.sh
@@ -1,43 +1,15 @@
 #! /bin/bash
 
 #===================================================
-# Bash script for .desktop Creation
+# Bash script for dotDesktop menu
 # Author: Raimundo Nonato
 # version: 1.3
 #===================================================
 
-#Own desktop file configuration
+source fileCreator.sh
 
-APPLICATIONSPATH="$HOME/.local/share/applications/"
-MYDESKTOPFILE="dotDesktop.desktop"
-
-# Own desktop file verification
-
-function firstRun() {
-    if [ -f "$APPLICATIONSPATH$MYDESKTOPFILE" ] 
-    then 
-        echo "Everything is alright!"   
-    else 
-        zenity --question --title="dotDesktop" --text="There is no .desktop file to this application, want create one?"
-
-        echo $?
-
-        if [ "$?" = "0" ]
-        then
-            NAME="dotDesktop"
-            VERSION="1.3"
-            COMMENT="A .desktop file creator"
-            TYPE="Application"
-            FILEPATH=$(realpath -e dotDesktop.sh)
-            ICON=$(realpath -e icon.png)
-            CATEGORIES="System;Utility"
-
-            APPLICATIONDESKTOP="$APPLICATIONSPATH$MYDESKTOPFILE"
-
-            createFile
-        fi
-    fi
-}
+# Verify if there a .desktop file for the application
+firstRun
 
 function menu() {
     CHOICE=$(zenity --list --window-icon=question --title="dotDesktop Menu" --text="Welcome to dotDesktop\nWhat you want do?" --column="Action" "Create" "exit")
@@ -54,64 +26,4 @@ function menu() {
 
 }
 
-function getInfo() {
-
-    NAME=$(zenity --entry --title="Application name" --text="Name(Obligatory):")
-
-    VERSION=$(zenity --entry --title="Application version" --text="Application version")
-
-    COMMENT=$(zenity --entry --title="Application comment" --text="Comment")
-
-    TYPE=$(zenity --list --window-icon=question --title="Application type" --text="(Obligatory)" --column="Type" "Application" "Link" "Directory")
-
-    FILEPATH=$(zenity --file-selection --title="Application path")
-
-    ICON=$(zenity --file-selection --file-filter="*.png *.svg" --title="Application icon")
-
-    CATEGORIES=$(zenity --list \
-                        --multiple \
-                        --text="Select the categories(More than one can be selected!)" \
-                        --title="Application categoria" \
-                        --column="Categories" "AudioVideo" "Development" "Education" "Game" "Graphics" "Network" "Office" "Science" "Settings" "System" "Utility" \
-                        --separator=";")
-
-    APPLICATIONDESKTOP="$APPLICATIONSPATH$NAME.desktop"
-
-    createFile
-}
-
-function createFile() {
-
-        if [ -f "$APPLICATIONDESKTOP" ]
-        then
-            zenity --question --title="Overwrite" --text="File already exist, overwrite?"
-
-            if [ "$?" = "0" ]
-            then
-                rm -rf $APPLICATIONDESKTOP       
-            else
-                zenity --info --text="File Creation canceled!"
-
-                menu
-            fi
-        else
-                touch $APPLICATIONDESKTOP
-
-                echo "[Desktop Entry]" >> $APPLICATIONDESKTOP
-                echo "" >> $APPLICATIONDESKTOP
-                echo "Name=$NAME" >> $APPLICATIONDESKTOP
-                echo "Version=$VERSION" >> $APPLICATIONDESKTOP
-                echo "Comment='$COMMENT'" >> $APPLICATIONDESKTOP
-                echo "Type=$TYPE" >> $APPLICATIONDESKTOP
-                echo "Exec='$FILEPATH'" >> $APPLICATIONDESKTOP
-                echo "Icon=$ICON" >> $APPLICATIONDESKTOP
-                echo "Categories=$CATEGORIES;" >> $APPLICATIONDESKTOP
-
-                zenity --info --text=".Desktop file created!"
-
-                menu
-        fi
-}
-
-firstRun
 menu
